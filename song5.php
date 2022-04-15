8b4f7a303106b08877bcbe55da211ccf4a1adfd8
Mhelliscent M. Ollado <olladokim20@gmail.com>
2022-04-15 23:50:15 +0800

    my song 5

---
+++ song5.php
@@ -0,0 +1,115 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Song5-Common Denominator-Justin Bieber</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Tambay Tunes and Lyrics </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Tambay Tunes and Lyrics</h1>
+                <p class="lead">With music you'll have your own world.</p>
+
+                                                                                                                                                                                                                                                                                                                                                                                                          
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php
+                        echo "<h2 class='text-primary'>Common Denominator</h2>";
+                        echo "<p class='lead'>Justin Bieber</p>";
+
+                        echo "<p>Just a fraction of your love fills the air</p>";
+                        echo "<p>And I fall in love with you all over again</p>";
+                        echo "<p>You're the light that feeds the sun in my world</p>";
+                        echo "<p>I'd face a thousand years of pain for my girl</p>";
+
+                        echo "<p>Out of all the things in life that I could fear</p>";
+                        echo "<p>The only thing that would hurt me is if you weren't here</p>";
+
+                        echo "<p>I don't wanna go back to just being one half of the equation</p>";
+                        echo "<p>Do you understand what I'm sayin'?</p>";
+                        echo "<p>Girl, without you I'm lost</p>";
+                        echo "<p>Can't fix this compass at heart</p>";
+                        echo "<p>Between me and love</p>";
+                        echo "<p>You're the common denominator</p>";
+                        echo "<p>You're the common denominator</p>";
+
+                        echo "<p>Before you love was low</p>";
+                        echo "<p>Playing girls was my high</p>";
+                        echo "<p>You changed the game</p>";
+                        echo "<p>Now I put my card to the side</p>";
+                        echo "<p>When broken hearts rise up to say, Love is a lie.</p>";
+                        echo "<p>You and I will stand to be love's reply</p>";
+
+                        echo "<p>Out of all the things in life that I could fear</p>";
+                        echo "<p>The only thing that would hurt me is if you weren't here, here, here</p>";
+
+                        echo "<p>I don't wanna go back to just being one half of the equation</p>";
+                        echo "<p>Do you understand what I'm sayin'?</p>";
+                        echo "<p>Girl, without you I'm lost</p>";
+                        echo "<p>Can't fix this compass at heart</p>";
+                        echo "<p>Between me and love</p>";
+                        echo "<p>You're the common denominator</p>";
+                        echo "<p>You're the common denominator</p>";
+
+                        echo "<p>I can't imagine life without your touch</p>";
+                        echo "<p>And every kiss that you give,</p>";
+                        echo "<p>You fill me up</p>";
+                        echo "<p>And through all the heart achin'</p>";
+                        echo "<p>Jealous females hatin'</p>";
+                        echo "<p>I'mma hold it down for you, you</p>";
+                        echo "<p>You're the common denominator</p>";
+
+                        echo "<p>I don't wanna go back...</p>";
+
+                        echo "<p>I don't wanna go back to just being one half of the equation</p>";
+                        echo "<p>Do you understand what I'm sayin'?</p>";
+                        echo "<p>Girl, without you I'm lost</p>";
+                        echo "<p>Can't fix this compass at heart</p>";
+                        echo "<p>Between me and love,</p>";
+                        echo "<p>You're the common denominator</p>";
+                        echo "<p>You're the common denominator</p>";
+
+                        echo "<p>Just a fraction of your love fills the air</p>";
+                        echo "<p>And I fall in love with you all over again</p>";
+                        ?>
+
+                        Back to main page <a href = "index.php">here!</a>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Mhelliscent M. Ollado</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
