87758bd7655258941cbe20265d79a96ae9754162
Mhelliscent M. Ollado <olladokim20@gmail.com>
2022-04-15 17:20:16 +0800

    my first song commit

--- index.php
+++ index.php
@@ -14,15 +14,16 @@
         <!-- Navigation-->
         <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
             <div class="container px-4">
-                <a class="navbar-brand" href="#page-top"> Add your brand name here </a>
+                <a class="navbar-brand" href="#page-top"> Tambay Tunes and Lyrics </a>
                 <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
                 <div class="collapse navbar-collapse" id="navbarResponsive">
                     <ul class="navbar-nav ms-auto">
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 1</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 2</a></li>
-                        <li class="nav-item"><a class="nav-link text-light" href="#">Song 3</a></li>
-                         <li class="nav-item"><a class="nav-link text-light" href="#">Song 4</a></li>
-                          <li class="nav-item"><a class="nav-link text-light" href="#">Song 5</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song1.php">Song 1</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song2.php">Song 2</a></li>
+                        <li class="nav-item"><a class="nav-link text-light" href="song3.php">Song 3</a></li>
+                         <li class="nav-item"><a class="nav-link text-light" href="song4.php">Song 4</a></li>
+                          <li class="nav-item"><a class="nav-link text-light" href="song5.php">Song 5</a></li>
+
                     </ul>
                 </div>
             </div>
@@ -30,9 +31,10 @@
         <!-- Header-->
         <header class="bg-dark bg-gradient text-white">
             <div class="container px-4 text-center">
-                <h1 class="fw-bolder">Welcome to Melomaniac</h1>
-                <p class="lead">Add a short description here</p>
-                
+                <h1 class="fw-bolder">Welcome to Tambay Tunes and Lyrics</h1>
+                <p class="lead">With music you'll have your own world.</p>
+
+                                                                                                                                                                                                                                                                                                                                                                                                          
             </div>
         </header>
         <!-- About section-->
@@ -40,16 +42,159 @@
             <div class="container px-4 ">
                 <div class="row gx-4 justify-content-center">
                     <div class="col-lg-12 text-center">
-                        <h2 class="text-primary">Song Title here</h2>
-                        <p class="lead">display the Composer here</p>
-                        <p>display the lyrics/chords here</p>
+
+                        <?php
+                        echo "<h2 class='text-primary'>Bazinga</h2>";
+                        echo "<p class='lead'>SB19</p>";
+
+                        echo "<p>Every day, I think the load gets heavy</p>";
+                        echo "<p>Throw it away, the world unloads hostility</p>";
+                        echo "<p>Is there away to break the curse and stop this now</p>";
+                        echo "<br/>";
+
+                        echo "<p>I'm going home</p>";
+                        echo "<p>Then I'll cry alone</p>";
+                        echo "<p>Don't you worry</p>";
+                        echo "<p>Soon, I'll be gone</p>";
+                        echo "<br/>";
+
+                        echo "<p>Psyche!</p>";
+                        echo "<br/>";
+
+                        echo "<p>Bazinga!</p>";
+                        echo "<p>Hate niyo'y gasolina<?p>";
+                        echo "<p>Yeah, I'm fire I linger</p>";
+                        echo "<p>Dare you turn up the heat<p>";
+                        echo "<p>And I'll burn y'all till I die</p>";
+                        echo "<p>Yeah, Bruce be proud</p>";
+                        echo "<p>I'm water</p>";
+                        echo "<p>Moves, they stagger</p>";
+                        echo "<p>Wattah!</p>";
+                        echo "<p>Buzz killer, I'm killing your buzz</p>";
+                        echo "<p>So get the hell out</p>";
+                        echo "<br/>";
+
+                        echo "<p>Clown!</p>";
+                        echo "<p>Who's the boss now?</p>";
+                        echo "<p>I just turned the world upside down</p>";
+                        echo "<p>Now, y'all can quit thinking cos you won't know how</p>";
+                        echo "<p>I did it, bounced and kicked it</p>";
+                        echo "<p>Rounndhouse</p>";
+                        echo "<p>Ya Ya!</p>";
+                        echo "<br/>";
+
+                        echo "<p>Don't yous see don't you see</p>";
+                        echo "<p>this is my legacy</p>";
+                        echo "<p>Now, how y'all gonna get to me</p>";
+                        echo "<p>When I got the almighty with me</p>";
+                        echo "<p>The bibidi babidi boo</p>";
+                        echo "<br/>";
+
+                        echo "<p>Everyday, I think the loads gets heavy</p>";
+                        echo "<p>Throws it away, the world unloads hostility</p>";
+                        echo "<p>Is there a way to break the curse and stop this now</p>";
+                        echo "<br/>";
+
+                        echo "<p>Go throw a stone</p>";
+                        echo "<p>It might break a bone</p>";
+                        echo "<p>But be wary because I won't condone</p>";
+                        echo "<br/>";
+
+                        echo "<p>Bazinga!</p>";
+                        echo "<p>Hate nyo'y gasolina</p>";
+                        echo "<p>Yeah, I'm fire I linger</p>";
+                        echo "<p>Dare you to turn up the heat</p>";
+                        echo "<p>And I'll burn y'all till I die</p>";
+                        echo "<p>Yeah, Bruce be proud</p>";
+                        echo "<p>I'm water</p>";
+                        echo "<p>Moves, they stagger</p>";
+                        echo "<p>Wattah!</p>";
+                        echo "<p>Buzz killer, I'm killing your buzz</p>";
+                        echo "<p>So get the hell out!</p>";
+                        echo "<br/>";
+
+                        echo "<p>Look at you</p>";
+                        echo "<p>Look at you</p>";
+                        echo "<br/>";
+
+                        echo "<p>Look at you</p>";
+                        echo "<p>Look at you</p>";
+                        echo "<br/>";
+
+                        echo "<p>Look at you</p>";
+                        echo "<p>Peek a Boo</p>";
+                        echo "<p>Pick a bpp-boo</p>";
+                        echo "<p>I'mma beat you</p>";
+                        echo "<p>Even if I didn't learn Kung Fu</p>";
+                        echo "<p>1 2 punch ooh</p>";
+                        echo "<p>You're lucky cos I over did with</p>";
+                        echo "<p>Had to say that 2</p>";
+
+                        echo "<p>Not a mob, just a psycho</p>";
+                        echo "<p>I'mma keep it low</p>";
+                        echo "<p>Guess you know that I am the one</p>";
+                        echo "<p>I dare you to double O</p";
+                        echo "<br/>";
+
+                        echo "<p>Lemme get this straight</p>";
+                        echo "<p>Just got a few more words to say</p>";
+                        echo "<p>Defibrillator to your ear</p>";
+                        echo "<p>Til I won't have to say</p>";
+                        echo "<p>Clear</p>";
+                        echo "<br/>";
+
+                        echo "<p>Bakit ba hilig nyo mamuna</p>";
+                        echo "<p>Masakit na sa tenga pag nagbara (urasai)</p>";
+                        echo "<p>Wag mag-alala lahat tanda ko pa (wasurenai)</p>";
+                        echo "<p>Lahat ng mga pambihirang natamasa</p>";
+                        echo "<p>Naku pasensya na</p>";
+                        echo "<br/>";
+
+                        echo "<p>Last time, I thought I already</p>";
+                        echo "<p>Told you that I'm always ready</p>";
+                        echo "<p>Come on and test me, you know</p>";
+                        echo "<p>Nothing's gonna stop me</p>";
+                        echo "<p>Bring it all out now</p>";
+                        echo "<br/>";
+
+                        echo "<p>All doors are close</p>";
+                        echo "<p>And no light unfolds</p>";
+                        echo "<p>So I'll break the dawn in my home</p>";
+                        echo "</br>";
+
+                        echo "<p>Bazinga!</p>";
+                        echo "<p>Hate nyo'y gasolina</p>";
+                        echo "<p>Yeah, I'm fire I linger</p>";
+                        echo "<p>Dare you to turn up the heat</p>";
+                        echo "<p>And I'll burn y'all till I die</p>";
+                        echo "<p>Yeah, Bruce be proud</p>";
+                        echo "<p>I'm water</p>";
+                        echo "<p>Moves, they stagger</p>";
+                        echo "<p>Wattah!</p>";
+                        echo "<p>Buzz killer, I'm killing your buzz</p>";
+                        echo "<p>So get the hell out!</p>";
+                        echo "<br/>";
+
+                        echo "<p>Look at you, look at you</p>";
+                        echo "<p>Look at you, look at you</p>";
+                        echo "<p>Look at you, look at you</p>";
+                        echo "<p>Look at you, look at you</p>";
+                        echo "<br/>";
+
+                        echo "<p>K.O</p>";
+                        echo "<p>SB19 wins!</p>";
+                        ?>
+
+
+
+
                     </div>
                 </div>
             </div>
         </section>
 
         <footer class="py-3 bg-danger">
-            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. add your fullname here</p></div>
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. add Mhelliscent M. Ollado</p></div>
         </footer>
         <!-- Bootstrap core JS-->
         <script src="js/bootstrap.bundle.min.js"></script>
