2f35304273a23f2cacb9c861ad0209dc4c192490
Mhelliscent M. Ollado <olladokim20@gmail.com>
2022-04-15 21:03:41 +0800

    my song 3

---
+++ song3.php
@@ -0,0 +1,127 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Song3-Ikako-SB19</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Tambay Tunes and Lyrics </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Tambay Tunes and Lyrics</h1>
+                <p class="lead">With music you'll have your own world.</p>
+
+                                                                                                                                                                                                                                                                                                                                                                                                          
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php
+                        echo "<h2 class='text-primary'>Ikako</h2>";
+                        echo "<p class='lead'>SB19</p>";
+
+                        echo "<p>Pag-gising sa umaga'y</p>";
+                        echo "<p>Tinig mo ang paboritong himig</p>";
+                        echo "<p>'Di naman siguro 'to isang panaginip</p>";
+                        echo "<br/>";
+
+                        echo "<p>Pinagdarasal parati kaligtasan mo't mapawi</p>";
+                        echo "<p>Ang lungkot sayong mata,</p>";
+                        echo "<p>'Di na luluha pang muli</p>";
+                        echo "<br/>";
+
+                        echo "<p>Panahon ma'y nagdidilim</p>";
+                        echo "<p>Liwanag ay sisikat din</p>";
+                        echo "<p>Sabay nating haharapin</p>";
+                        echo "<p>Takot sa puso'y alisin</p>";
+                        echo "<p>Lahat ng to'y lilipas din</p>";
+                        echo "<p>Walang pagsubok na 'di kakayanin</p>";
+                        echo "<br/>";
+
+                        echo "<p>Ibig ko lang ipahiwatig</p>";
+                        echo "<p>Na basta may pag ibig</p>";
+                        echo "<p>Sa puso nati'y magwawagi</p>";
+                        echo "<p>O, Sigurado</p>";
+                        echo "<p>Babangon tayo</p>";
+                        echo "<p>Kaya'ng lahat</p>";
+                        echo "<p>Ikaw at ako</p>";
+                        echo "<p>'Wag kang susuko</p>";
+                        echo "<p>Magkasama tayo</p>";
+                        echo "<p>Hanggang sa dulo</p>";
+                        echo "<p>Ikaw at ako</p>";
+                        echo "<br/>";
+
+                        echo "<p>Ohh ohhhh</p>";
+                        echo "<br/>";
+
+                        echo "<p>Nananana</p>";
+                        echo "<br/>";
+
+                        echo "<p>Panahon ma'y nagdidilim</p>";
+                        echo "<p>Liwanag ay sisikat din</p>";
+                        echo "<p>Sabay nating haharapin</p>";
+                        echo "<p>Takot sa puso'y alisin</p>";
+                        echo "<p>Lahat ng to'y lilipas din</p>";
+                        echo "<p>Walang pagsubok na 'di kakayanin</p>";
+                        echo "<br/>";
+
+                        echo "<p>Ibig ko lang ipahiwatig</p>";
+                        echo "<p>Na basta may pag ibig</p>";
+                        echo "<p>Sa puso nati'y magwawagi</p>";
+                        echo "<br/>";
+
+                        echo "<p>O, Sigurado</p>";
+                        echo "<p>Babangon tayo</p>";
+                        echo "<p>Kaya'ng lahat</p>";
+                        echo "<p>Ikaw at ako</p>";
+                        echo "<p>'Wag kang susuko</p>";
+                        echo "<p>Magkasama tayo</p>";
+                        echo "<p>Hanggang sa dulo</p>";
+                        echo "<p>Ikaw at ako</p>";
+                        echo "<br/>";
+
+                        echo "<p>Ohh ohhhh</p>";
+                        echo "<br/>";
+
+                        echo "<p>Nananana</p>";
+                        echo "<br/>";
+
+                        ?>
+
+                        Back to main page <a href = "index.php">here!</a>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Mhelliscent M. Ollado</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
