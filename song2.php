62914eb644c23100207fc36715e7356536aed038
Mhelliscent M. Ollado <olladokim20@gmail.com>
2022-04-15 19:33:03 +0800

    my song 2

---
+++ song2.php
@@ -0,0 +1,156 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Song2-SLMT-SB19</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Tambay Tunes and Lyrics </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Tambay Tunes and Lyrics</h1>
+                <p class="lead">With music you'll have your own world.</p>
+
+                                                                                                                                                                                                                                                                                                                                                                                                          
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php
+                        echo "<h2 class='text-primary'>SLMT</h2>";
+                        echo "<p class='lead'>SB19</p>";
+
+                        echo "<p>Ako'y nagpapasalamat</p>";
+                        echo "<p>Sa lahat ng sumabay sa paglalakbay ko</p>";
+                        echo "<p>Papatungo sa 'king pangarap</p>";
+                        echo "<p>Lagi ko kayong tanaw sa paglipad</p>";
+                        echo "<br/>";
+
+                        echo "<p>Oh-oh-oh</p>";
+                        echo "<p>Oh-oh-oh, oh-oh-oh-oh</p>";
+                        echo "<p>Oh-oh-oh</p>";
+                        echo "<p>Oh-oh-oh, oh-oh-oh-oh</p>";
+                        echo "<br/>";
+
+                        echo "<p>Sabi nila, 'di ko raw kaya (hmm)</p>";
+                        echo "<p>'Lang mapapala, wala raw pag-asa</p>";
+                        echo "<p>Bukambibig ng madla'y puro panggagaya</p>";
+                        echo "<p>Pero salamat sa lahat ng naniwala</p>";
+                        echo "<br/>";
+
+                        echo "<p>Naniwala, nawala lahat ng pangamba</p>";
+                        echo "<p>Nagsimula magmula nu'ng makasama ka</p>";
+                        echo "<p>'Di ko na kailangan pa ng iba</p>";
+                        echo "<p>Halika, may sikreto ako, atin-atin lang (Sing it!)</p>";
+                        echo "<br/>";
+
+                        echo "<p>'Lam mo ba? 'Di makakaya nang wala ka, yeah</p>";
+                        echo "<p>Walang makakatumbas sa atin kahit ano pa mang halaga</p>";
+                        echo "<br/>";
+
+                        echo "<p>Ako'y nagpapasalamat (Thanks!)</p>";
+                        echo "<p>Sa lahat ng sumabay sa paglalakbay ko</p>";
+                        echo "<p>Patungo sa 'king pangarap (Hey!)</p>";
+                        echo "<p>Lagi ko kayong tanaw sa paglipad</p>";
+                        echo "<p>Ako'y nagpapasalamat (Thanks!)</p>";
+                        echo "<p>Sa mga naniwala sa 'king abilidad</p>";
+                        echo "<p>Sa paggapang ko na banayad (Hey!)</p>";
+                        echo "<p>Tatanawin ko kayo sa 'king paglipad</p>";
+                        echo "<br/>";
+
+                        echo "<p>Salamat, sa-salamat, salamat</p>";
+                        echo "<p>Thank you very much</p>";
+                        echo "<p>Salamat, sa-salamat, salamat</p>";
+                        echo "<p>Thank you very much</p>";
+                        echo "<br/>";
+
+                        echo "<p>Please lang, 'wag ka nang umalis, love</p>";
+                        echo "<p>Kasi lagi-lagi na kitang nami-miss lang</p>";
+                        echo "<p>Parang 'di nagbayad, 1, 2, 3, hanggang peace lang</p>";
+                        echo "<p>Dapat paramihin mo ng anim 'yung tatlo para unlimited</p>";
+                        echo "<br/>";
+
+                        echo "<p>Ang samahan, 'lang hanggan</p>";
+                        echo "<p>Kahit pa sukdulan ang mga karanasan</p>";
+                        echo "<p>Walang 'di kayang lagpasan</p>";
+                        echo "<p>Kahit pa sa'n, ako ay lagi mong pasan</p>";
+                        echo "<p>Minsan may bardagulan</p>";
+                        echo "<br/>";
+
+                        echo "<p>'Lang labing-siyam 'pag walang labing-walong mga kayamanan</p>";
+                        echo "<p>'Yan ang dahilan ng matamis na karanasan</p>";
+                        echo "<p>Kahit pa umulan ng kamalasan</p>";
+                        echo "<p>'Di ko na namalayan kasi lagi kang nariyan, yeah</p>";
+                        echo "<br/>";
+
+                        echo "<p>Ako'y nagpapasalamat (Thanks!)</p>";
+                        echo "<p>Sa lahat ng sumabay sa paglalakbay ko</p>";
+                        echo "<p>Patungo sa 'king pangarap (Hey!)</p>";
+                        echo "<p>Lagi ko kayong tanaw sa paglipad</p>";
+                        echo "<p>Ako'y nagpapasalamat (Thanks!)</p>";
+                        echo "<p>Sa mga naniwala sa 'king abilidad</p>";
+                        echo "<p>Sa paggapang ko na banayad (Hey!)</p>";
+                        echo "<p>Tatanawin ko kayo sa 'king paglipad</p>";
+                        echo "<br/>";
+
+                        echo "<p>Salamat, sa-salamat, salamat</p>";
+                        echo "<p>Thank you very much</p>";
+                        echo "<p>Salamat, sa-salamat, salamat</p>";
+                        echo "<p>Thank you very much</p>";
+                        echo "<br/>";
+
+                        echo "<p>Palagi mang ma-hopia, 'di ka nagsasawa</p>";
+                        echo "<p>S-L-M-T, nakuha mo ba? Ikaw 'yung A</p>";
+                        echo "<p>Palagi mang ma-hopia, 'di ka nagsasawa</p>";
+                        echo "<p>S-L-M-T nang sobra, mahalima, mahal ka ng lima</p>";
+                        echo "<br/>";
+
+                        echo "<p>Oh-oh-oh (Salamat)</p>";
+                        echo "<p>Oh-oh-oh, oh-oh-oh-oh (Salamat)</p>";
+                        echo "<p>Oh-oh-oh (Oh)</p>";
+                        echo "<p>Oh-oh-oh, oh-oh-oh-oh (Salamat)</p>";
+                        echo "<p>Oh-oh-oh (Hmm)</p>";
+                        echo "<p>Oh-oh-oh, oh-oh-oh-oh (Oh)</p>";
+                        echo "<p>Oh-oh-oh (Hmm)</p>";
+                        echo "<p>Oh-oh-oh, oh-oh-oh-oh</p>";
+                        echo "<br/>";
+
+                        ?>
+
+                        Back to main page <a href = "index.php">here!</a>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Mhelliscent M. Ollado</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
