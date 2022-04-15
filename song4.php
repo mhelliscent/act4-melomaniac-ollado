3787ca63597983358314bf19cde4f0366f6a0a99
Mhelliscent M. Ollado <olladokim20@gmail.com>
2022-04-15 23:38:53 +0800

    my song 4

---
+++ song4.php
@@ -0,0 +1,118 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <meta charset="utf-8" />
+        <meta name="viewport" content="width=device-width, initial-scale=1, shrink-to-fit=no" />
+        <meta name="description" content="" />
+        <meta name="author" content="" />
+        <title>Song4-More Than Meets the Eye-Kolohe Kai</title>
+        <link rel="icon" type="image/x-icon" href="assets/favicon.ico" />
+      
+        <link href="css/styles.css" rel="stylesheet" />
+    </head>
+
+    <body id="page-top">
+        <!-- Navigation-->
+        <nav class="navbar navbar-expand-lg navbar-dark bg-primary fixed-top" id="mainNav">
+            <div class="container px-4">
+                <a class="navbar-brand" href="#page-top"> Tambay Tunes and Lyrics </a>
+                <button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarResponsive" aria-controls="navbarResponsive" aria-expanded="false" aria-label="Toggle navigation"><span class="navbar-toggler-icon"></span></button>
+                <div class="collapse navbar-collapse" id="navbarResponsive">
+
+                </div>
+            </div>
+        </nav>
+        <!-- Header-->
+        <header class="bg-dark bg-gradient text-white">
+            <div class="container px-4 text-center">
+                <h1 class="fw-bolder">Welcome to Tambay Tunes and Lyrics</h1>
+                <p class="lead">With music you'll have your own world.</p>
+
+                                                                                                                                                                                                                                                                                                                                                                                                          
+            </div>
+        </header>
+        <!-- About section-->
+        <section id="songs">
+            <div class="container px-4 ">
+                <div class="row gx-4 justify-content-center">
+                    <div class="col-lg-12 text-center">
+
+                        <?php
+                        echo "<h2 class='text-primary'>More Than Meets the Eye</h2>";
+                        echo "<p class='lead'>Kolohe Kai</p>";
+
+                        echo "<p>Prettiness & charm can sometimes be in vain</p>";
+                        echo "<p>But a loyal love from you I see pouring out like rain, yeah</p>";
+                        echo "<p>Oh girl you got this smile that makes my world go round</p>";
+                        echo "<p>And for an awful lonely while I never thought your love could be found</p>";
+                        echo "<br/>";
+
+                        echo "<p>I think it's safe to say</p>";
+                        echo "<p>That I know you I really do</p>";
+                        echo "<p>How things turned out this way</p>";
+                        echo "<p>I never thought you could be so true</p>";
+                        echo "<p>I'm thankful everyday</p>";
+                        echo "<p>That I truly know the girl that I adore</p>";
+                        echo "<br/>";
+
+                        echo "<p>Oh it's the first time</p>";
+                        echo "<p>That I liked a girl before I saw good-looks yeah</p>";
+                        echo "<p>Oh how you shook my heart</p>";
+                        echo "<p>It's the first time</p>";
+                        echo "<p>That I liked a girl who loves to read her books yeah</p>";
+                        echo "<p>My heart you took from the start</p>";
+                        echo "<p>Oh when I hear your wisest words it takes my heart</p>";
+                        echo "<p>Higher than the birds fly</p>";
+                        echo "<p>Oh with you girl there's so much more than meets the eye</p>";
+                        echo "<br/>";
+
+                        echo "<p>Some say beauty is only skin deep and its true</p>";
+                        echo "<p>But when I look at you I see beauty all the way through, yeah</p>";
+                        echo "<p>Girl can't you just see what's happening to me</p>";
+                        echo "<p>These butterflies inside are finally breaking free</p>";
+                        echo "<p>Your humility, oh how it refreshes my soul</p>";
+                        echo "<p>And your loyalty shows your heart glitter like gold</p>";
+                        echo "<p>Baby there's no doubt that I really know you inside out</p>";
+                        echo "<br/>";
+
+                        echo "<p>Oh it's the first time</p>";
+                        echo "<p>That I liked a girl before I saw good-looks yeah</p>";
+                        echo "<p>Oh how you shook my heart</p>";
+                        echo "<p>It's the first time</p>";
+                        echo "<p>That I liked a girl who loves to read her books yeah</p>";
+                        echo "<p>My heart you took from the start</p>";
+                        echo "<p>Oh when I hear your wisest words it takes my heart</p>";
+                        echo "<p>Higher than the birds fly</p>";
+                        echo "<p>Oh with you girl there's so much more than meets the eye</p>";
+                        echo "<br/>";
+
+                        echo "<p>Oh it's the first time</p>";
+                        echo "<p>That I liked a girl before I saw good-looks yeah</p>";
+                        echo "<p>Oh how you shook my heart</p>";
+                        echo "<p>It's the first time</p>";
+                        echo "<p>That I liked a girl who loves to read her books yeah</p>";
+                        echo "<p>My heart you took from the start</p>";
+                        echo "<p>Oh when I hear your wisest words it takes my heart</p>";
+                        echo "<p>Higher than the birds fly</p>";
+                        echo "<p>Oh with you girl, oh with you girl, oh with you girl</p>";
+                        echo "<p>There's so much more than meets the eye</p>";
+                        echo "<br/>";
+
+                        ?>
+
+                        Back to main page <a href = "index.php">here!</a>
+
+                    </div>
+                </div>
+            </div>
+        </section>
+
+        <footer class="py-3 bg-danger">
+            <div class="container px-4"><p class="m-0 text-center text-white">Copyright &copy; 2022. Mhelliscent M. Ollado</p></div>
+        </footer>
+        <!-- Bootstrap core JS-->
+        <script src="js/bootstrap.bundle.min.js"></script>
+        <!-- Core theme JS-->
+        <script src="js/scripts.js"></script>
+    </body>
+</html>
